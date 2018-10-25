c16f0e4b987e75539e01efb96786c1e4cd5bbf82
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2018-10-25 21:20:53 +0200

    Add welome panel, menu and basdic styling for the landing page

--- api/api.js
+++ api/api.js
@@ -15,7 +15,8 @@ export const getBackgrounds = async () => {
 };
 
 export const getImage = (photoId) => {
-    const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${process.env.FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
+    const FLICKR_API_KEY = '294f34ea89e701cd06bf7f63d4b7e8c5';
+    const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
 
     return axios.get(flickrUrl)
     .then(data => { 
--- index.vr.js
+++ index.vr.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { AppRegistry, asset, Pano, Text, View } from "react-vr";
+import { AppRegistry, asset, Pano, Text, View, StyleSheet } from "react-vr";
 import { getImages, getBackgrounds } from "./api/api";
-require('dotenv').config();
 
 export default class react_vr_demo extends React.Component {
   constructor() {
@@ -15,7 +14,6 @@ export default class react_vr_demo extends React.Component {
   }
 
   componentDidMount() {
-
     getBackgrounds().then(backgroundImages => {
       this.setState({
         background: backgroundImages[0],
@@ -23,31 +21,77 @@ export default class react_vr_demo extends React.Component {
         currentDestination: 0
       });
     });
+  }
 
+  travelToDestination(index) {
+    console.log("travelToDestination", index);
   }
 
   render() {
+    const destinationDescriptions = ["Flood", "City by night"];
+
     return (
       <View>
         <Pano source={{ uri: this.state.background }} />
-        <Text
-          style={{
-            backgroundColor: "#777879",
-            fontSize: 0.8,
-            fontWeight: "400",
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: "center",
-            textAlignVertical: "center",
-            transform: [{ translate: [0, 0, -3] }]
-          }}
-        >
-          hello
-        </Text>
+        <Text style={styles.welcomePanel}>React VR adventure traveller</Text>
+        <View style={styles.menuWrapper}>
+          {this.state.backgroundImages.map((destination, index) => {
+            return (
+              <View
+                style={styles.menuItem}
+                key={index}
+                onEnter={() => {
+                  this.travelToDestination(index);
+                }}
+              >
+                <Text style={styles.menuItemText}>
+                  {destinationDescriptions[index]}
+                </Text>
+              </View>
+            );
+          })}
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  welcomePanel: {
+    backgroundColor: "#ef5350",
+    fontSize: 0.8,
+    fontWeight: "400",
+    layoutOrigin: [0.5, 0.5],
+    paddingLeft: 0.2,
+    paddingRight: 0.2,
+    textAlign: "center",
+    textAlignVertical: "center",
+    transform: [{ translate: [0, 2, -5] }]
+  },
+  menuWrapper: {
+    width: 5,
+    height: 1.25,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    transform: [{ translate: [-2, 0, -7.5] }]
+  },
+  menuItem: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 2,
+    height: 2,
+    borderRadius: 2,
+    borderWidth: 0.04,
+    borderColor: "#fff",
+    backgroundColor: "#ef5350"
+  },
+  menuItemText: {
+    fontSize: 0.4,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff"
+  }
+});
 
 AppRegistry.registerComponent("react_vr_demo", () => react_vr_demo);
