97c9cadf03c446b52032e4c6aca8ccee2de3a77a
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2018-10-25 20:25:52 +0200

    Use promise.all to fetch all backgrounds from flickr and set them in the application state

--- api/api.js
+++ api/api.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
 
-export const getImages = (photoId) => {
-    const FLICKR_API_KEY = '294f34ea89e701cd06bf7f63d4b7e8c5';
-    const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
+export const getBackgrounds = async () => {
+    const photoIds = [
+        34506405094,
+        5097827282,
+    ];
+    const promises = photoIds.map(id => getImage(id));
+
+    return Promise.all(promises)
+    .then(results => results)
+    .catch(err => {
+        console.error("when fetching background images", err);
+    });
+};
+
+export const getImage = (photoId) => {
+    const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${process.env.FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
 
     return axios.get(flickrUrl)
     .then(data => { 
--- index.vr.js
+++ index.vr.js
@@ -1,55 +1,53 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
-import { getImages } from './api/api';
+import React from "react";
+import { AppRegistry, asset, Pano, Text, View } from "react-vr";
+import { getImages, getBackgrounds } from "./api/api";
+require('dotenv').config();
 
 export default class react_vr_demo extends React.Component {
-
   constructor() {
     super();
-
     this.state = {
-      background: 'https://farm2.staticflickr.com/1763/41116404980_d4a20da144_b.jpg'
+      background:
+        "https://farm2.staticflickr.com/1763/41116404980_d4a20da144_b.jpg",
+      backgroundImages: [],
+      currentDestination: 0
     };
   }
 
   componentDidMount() {
-    getImages(34506405094)
-    .then(data => {
+
+    getBackgrounds().then(backgroundImages => {
       this.setState({
-        background: data
-      })
-      .catch(err => console.error(err));
+        background: backgroundImages[0],
+        backgroundImages,
+        currentDestination: 0
+      });
     });
+
   }
 
   render() {
-
     return (
       <View>
-        <Pano source={{uri: this.state.background}}/>
+        <Pano source={{ uri: this.state.background }} />
         <Text
           style={{
-            backgroundColor: '#777879',
+            backgroundColor: "#777879",
             fontSize: 0.8,
-            fontWeight: '400',
+            fontWeight: "400",
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
+            textAlign: "center",
+            textAlignVertical: "center",
+            transform: [{ translate: [0, 0, -3] }]
+          }}
+        >
           hello
         </Text>
       </View>
     );
   }
-};
+}
 
-AppRegistry.registerComponent('react_vr_demo', () => react_vr_demo);
+AppRegistry.registerComponent("react_vr_demo", () => react_vr_demo);
