4019cfdcde68a7f9e2bc56e59beef9bb97f5a545
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2018-10-31 21:59:19 +0100

    Include static assets in bundle

--- api/api.js
+++ api/api.js
@@ -24,8 +24,7 @@ export const getBackgrounds = async (destinations) => {
 };
 
 export const getImage = (photoId, id) => {
-  const FLICKR_API_KEY = "294f34ea89e701cd06bf7f63d4b7e8c5";
-  const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
+  const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${process.env.FLICKR_API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
 
   return axios
     .get(flickrUrl)
--- server.js
+++ server.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 7000
 
 connect().use(serveStatic(__dirname)).listen(PORT, function(){
     console.log(`Server running on ${PORT}`);
+    console.log(`Using key: ${process.env.FLICKR_API_KEY}`);
 });
